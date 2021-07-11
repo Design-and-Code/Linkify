c28dcc3e10b0a5965411b2e9dbda49704d9101f8
lostgirljourney <falgunisarkar526@gmail.com>
2021-07-11 15:25:30 +0530

    Added HeroSection

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import HeroSection from "../../components/HomePage/herosection/herosection";
 
-export default function App(){
-    return(
-        <div>
-            hi
-        </div>
-    )
+export default function App() {
+  return (
+    <HeroSection
+      titleP="All your links in one place."
+      about="The only link you'll ever need to connect with everyone in the world."
+      start="Get Started"
+    />
+  );
 }
