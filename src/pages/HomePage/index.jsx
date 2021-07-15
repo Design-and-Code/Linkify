121d1949ca3914ebefdf47d625e57f2621a2c6d7
nimishjn <nimishjain100701@gmail.com>
2021-07-16 01:21:34 +0530

    Footer added

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../components/HomePage/header/Header.js'
+import Footer from '../../components/HomePage/footer/footer.js'
 
 export default function App(){
     return(
         <div>
             <Header />
+            <Footer />
         </div>
     )
 }
