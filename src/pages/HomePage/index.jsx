5427a67afd38198ba519ccb442fb3c19d25d97a8
Pratham Krishna <34184758+Prathamkrishna@users.noreply.github.com>
2021-07-17 21:38:58 +0530

    Update index.jsx

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -8,12 +8,12 @@ export default function App(){
     return(
         <div>
             <Header />
-            <Home />
             <HeroSection
             titleP="All your links in one place."
             about="The only link you'll ever need to connect with everyone in the world."
             start="Get Started"
             />
+            <Home />
         </div>
     )
 }
