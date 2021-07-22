c38c83b3a2989ec8d020725bb9c8baf11ef9f3a0
Aniruddha Das <aniruddha.das.852@gmail.com>
2021-07-22 21:43:31 +0530

    Created Responsive Detailed Feature Section

---
+++ src/components/HomePage/features1/feature1.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./features1.module.css";
+import img from "./assets/feature_laptop.png";
+import cx from "classnames";
+
+const detailedFeature = () => {
+  return (
+    <section className={styles.detailedFeatureSection}>
+      <div className={cx(styles.row, styles.dashboard)}>
+        <div>
+          <img src={img} alt="" />
+        </div>
+        <div>
+          <h3>Easy to use Dashboard</h3>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia duis enim velit mollit. Amet minim mollit non
+            deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            duis enim velit mollit.
+          </p>
+        </div>
+      </div>
+      <div className={cx(styles.row, styles.customizable)}>
+        <div>
+          <img src={img} alt="" />
+        </div>
+        <div>
+          <h3>Highly Customizable</h3>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia duis enim velit mollit. Amet minim mollit non
+            deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            duis enim velit mollit.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default detailedFeature;
