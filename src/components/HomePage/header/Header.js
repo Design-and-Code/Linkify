cdbffd8f5868b3879385ceb4fe1ba160d8df580a
Shridhar-dev <shridharkamat10@gmail.com>
2021-10-01 10:48:42 +0530

    responsive errors fixed

--- src/components/HomePage/header/Header.js
+++ src/components/HomePage/header/Header.js
@@ -11,6 +11,7 @@ export default function Header() {
     margin-top: 29px;
     margin-right: 112px;
     margin-left: 128px;
+    flex-wrap:wrap;
     @media (max-width: 1200px) {
       //insert code later if required.
     }
@@ -18,6 +19,9 @@ export default function Header() {
       margin-top: 15px;
       margin-right: 15px;
       margin-left: 15px;
+    }
+    @media (max-width: 300px) {
+      justify-content:center;
     }
   `;
   const LogoText = styled.div`
@@ -27,6 +31,7 @@ export default function Header() {
   const ButtonGroup = styled.div`
     display: flex;
     cursor: pointer;
+    flex-wrap:wrap;
   `;
   const LeftButton = styled.div`
     margin-right: 5px;
