2155c453c52e8e3746a82d3532925141055f7bed
nimishjn <nimishjain100701@gmail.com>
2021-11-01 14:41:48 +0530

    refactor: Feature 1 section

--- src/components/HomePage/features1/feature1.js
+++ src/components/HomePage/features1/feature1.js
@@ -5,8 +5,6 @@ import cx from "classnames";
 import { LinkifyContext } from "../../../context";
 
 const DetailedFeature = () => {
-  const { theme } = React.useContext(LinkifyContext);
-
   return (
     <section className={styles.detailedFeatureSection}>
       <div className={cx(styles.row, styles.dashboard)}>
@@ -14,11 +12,7 @@ const DetailedFeature = () => {
           <img src={img} alt="" />
         </div>
         <div>
-          <h3
-            className={`${theme === "light-theme" ? "lightfont" : "darkfont"}`}
-          >
-            Easy to use Dashboard
-          </h3>
+          <h3>Easy to use Dashboard</h3>
           <p>
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia duis enim velit mollit. Amet minim mollit non
@@ -32,11 +26,7 @@ const DetailedFeature = () => {
           <img src={img} alt="" />
         </div>
         <div>
-          <h3
-            className={`${theme === "light-theme" ? "lightfont" : "darkfont"}`}
-          >
-            Highly Customizable
-          </h3>
+          <h3>Highly Customizable</h3>
           <p>
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia duis enim velit mollit. Amet minim mollit non
